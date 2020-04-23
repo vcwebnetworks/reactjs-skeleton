a59b193f9ce3d752a73773e7b93e4f99d07415bf
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-04-23 14:13:54 -0300

    chore: adiciona modulo de imagem para import

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,43 @@
+import * as React from 'react';
+
+// eslint-disable-next-line spaced-comment
 /// <reference types="react-scripts" />
+
+declare module '*.bmp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  >>;
+
+  const src: string;
+  export default src;
+}
