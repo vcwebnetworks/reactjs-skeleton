2cfb66d30191c62b7fdf4d363ef49631e1c314cf
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-04-19 23:29:34 -0300

    feat: passa o historia para prop de rotas
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+
+import history from '~/services/history';
 
 import ScrollReset from './components/ScrollReset';
 import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Routes />
       <ScrollReset />
-    </BrowserRouter>
+    </Router>
   );
 };
 
